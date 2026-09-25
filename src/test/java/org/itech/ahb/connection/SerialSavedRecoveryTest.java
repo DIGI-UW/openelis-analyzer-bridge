d7bbee08b1077039ac5c9639a1ba92540ea84239
Piotr Mankowski <pmanko@uw.edu>
2026-09-24 19:33:57 -0700

    test: defer serial reconnect coverage for Madagascar release

--- src/test/java/org/itech/ahb/connection/SerialSavedRecoveryTest.java
+++ src/test/java/org/itech/ahb/connection/SerialSavedRecoveryTest.java
@@ -28,6 +28,7 @@ import org.itech.ahb.outbox.OutboxTestSupport;
 import org.itech.ahb.profile.AnalyzerProfileCatalog;
 import org.itech.ahb.serial.SerialMessageHandler;
 import org.itech.ahb.serial.SerialPortListener;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.jupiter.api.io.TempDir;
@@ -53,6 +54,9 @@ class SerialSavedRecoveryTest {
   HttpServer receiver;
 
   @Test
+  @Disabled(
+    "Deferred for Madagascar release; restore via https://github.com/DIGI-UW/openelis-analyzer-bridge/issues/67"
+  )
   @Timeout(60)
   void absentDeviceRestoresWithoutBlockingPeerAndReconnectsWithoutAnotherActivation() throws Exception {
     Pair pair = new Pair(directory);
